6b65413b753020a57144cbcd200cc6c87483e15e
Yanqin Qualmann <yq.qualmann@gmail.com>
2024-12-31 13:38:28 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,59 +1,71 @@
-
-
-import React, { useState, useEffect } from 'react';
-import Quiz from './components/Quiz';
-import Home from './components/Home';
-import Navbar from './components/Navbar';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import Quiz from "./components/Quiz";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import "./App.css";
 
 function App() {
-  const [lightMode, setLightMode] = useState(() => JSON.parse(localStorage.getItem("lightMode")))
+  const [lightMode, setLightMode] = useState(() =>
+    JSON.parse(localStorage.getItem("lightMode"))
+  );
   const [isHome, setIsHome] = useState(true);
   const [formData, setFormData] = useState({
-    numberOfQuestions: '5',
-    category: '',
-    difficulty: '',
-    type: ''
-
+    numberOfQuestions: "5",
+    category: "",
+    difficulty: "",
+    type: "",
   });
   function toggleLightMode() {
-    setLightMode(prev => !prev)
+    setLightMode((prev) => !prev);
   }
   function toggleIsHome() {
-    setIsHome(prev => !prev);
+    setIsHome((prev) => !prev);
   }
   function handleFormChange(e) {
     const { name, value } = e.target;
 
-    setFormData(prev => {
+    setFormData((prev) => {
       return {
-        ...prev, 
-        [name]: value
+        ...prev,
+        [name]: value,
       };
     });
   }
 
   useEffect(() => {
-    localStorage.setItem('lightMode', JSON.stringify(lightMode));
+    localStorage.setItem("lightMode", JSON.stringify(lightMode));
   }, [lightMode]);
   return (
     <div className="App">
-      <Navbar lightMode={lightMode} toggle={toggleLightMode}/>
-      {
-        isHome
-          ?
-          <Home toggleIsHome={toggleIsHome} formData={formData}
-            handleFormChange={handleFormChange} lightMode={lightMode}
+      <Navbar lightMode={lightMode} toggle={toggleLightMode} />
+      {isHome ? (
+        <Home
+          toggleIsHome={toggleIsHome}
+          formData={formData}
+          handleFormChange={handleFormChange}
+          lightMode={lightMode}
+        />
+      ) : (
+        <Quiz
+          formData={formData}
+          toggleIsHome={toggleIsHome}
+          lightMode={lightMode}
+        />
+      )}
 
-          />
-          :
-          <Quiz formData={formData} toggleIsHome={toggleIsHome}
-            lightMode={lightMode}
-          />
-      }
-
-      <footer>QIN勤©2023 up&qu</footer>
+      <footer>
+        <p>
+          © {new Date().getFullYear()} by{" "}
+          <a
+            href="https://www.yanqin.de"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ textDecoration: "none" }}
+          >
+            QIN's code
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
